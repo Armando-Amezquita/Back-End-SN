f315419945c79fe3abdd7874f17663246438ecf9
EdgarCalel <edgar_kll@hotmail.com>
2022-01-19 01:53:31 -0600

    rutademodificar

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,10 +3,12 @@ const server = express();
 
 //IMPORTACION DE RUTAS
 const usuarioRoute =require('./usuario.route')
+const postRoute =require('./post.route')
+
 
 //USO DE RUTAS
-server.get('/otro', (req, res, next)=>{
-res.send('hola')
-} )
+
 server.use('/usuario', usuarioRoute)
+server.use('/publicaciones', postRoute)
+
 module.exports = server
