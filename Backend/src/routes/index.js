653b4f476a380cb74fbf12f552198a119fce4498
EdgarCalel <edgar_kll@hotmail.com>
2022-01-20 10:23:20 -0600

    cambios

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,15 +3,6 @@ const server = express();
 
 //IMPORTACION DE RUTAS
 const usuarioRoute =require('./usuario.route')
-<<<<<<< HEAD
-const perroRoute = require('./perro.route')
-
-//USO DE RUTAS
-server.use('/usuario', usuarioRoute)
-server.use('/perro', perroRoute)
-
-
-=======
 const postRoute =require('./post.route')
 
 
@@ -19,6 +10,5 @@ const postRoute =require('./post.route')
 
 server.use('/usuario', usuarioRoute)
 server.use('/publicaciones', postRoute)
->>>>>>> f315419945c79fe3abdd7874f17663246438ecf9
 
 module.exports = server
