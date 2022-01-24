c6472cb4df5e145a04c5bc32f907796628dba294
JhoanBurbano <jhoanburbano@unicauca.edu.co>
2022-01-24 00:28:47 -0500

    actualizacion userroute

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,7 +1,7 @@
 const express =require ('express');
 const server = express();
-const jwt = require('jsonwebtoken')
 //IMPORTACION DE RUTAS
+
 const usuarioRoute =require('./usuario.route')
 const postRoute =require('./post.route')
 
