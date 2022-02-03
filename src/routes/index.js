4011881f9a577c51fc96e5cb76014a9ae87ed783
JhoanBurbano <jhoanburbano@unicauca.edu.co>
2022-02-02 22:58:45 -0500

    implementacion del socket

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,15 @@ const server = express();
 const usuarioRoute =require('./usuario.route')
 const postRoute =require('./post.route')
 const uploadRoute = require('./upload.route')
+const user = require('./users.route')
+const conversation = require('./conversation.route')
+const message = require('./message.route')
 
 
 //USO DE RUTAS
-
+server.use("/users", user)
+server.use("/conversation", conversation)
+server.use("/message", message)
 server.use('/usuarios', usuarioRoute)
 server.use('/posts', postRoute)
 
