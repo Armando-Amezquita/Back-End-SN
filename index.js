6756c81444268a5116ef1f9208cc9fceb81432a0
JhoanBurbano <jhoanburbano@unicauca.edu.co>
2022-02-02 22:35:46 -0500

    implementacion del socket

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const routes = require('./src/routes');
 const server = express();
-const cors = require('cors')
+const cors = require('cors');
 server.name = 'API';
 
 require('./db');
@@ -11,7 +11,7 @@ server.use(express.json({ limit: '50mb' }));
 // server.use(cookieParser());
 
 server.use(morgan('dev'));
-server.use(cors())
+server.use(cors());
 
 //MANEJO DE ERRORES
 server.use((err, req, res, next) => {
@@ -28,37 +28,54 @@ server.use('/', routes);
 
 server.set('port', process.env.PORT || 3001);
 
-
-
-
-const http = require('http')
-const { Server } = require('socket.io')
+const http = require('http');
+const { Server } = require('socket.io');
 const ServerHTTP = http.createServer(server);
 
 const io = new Server(ServerHTTP, {
 	cors: {
 		origin: process.env.FRONT_DOMAIN
 	}
-})
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+	!users.some((user) => user.userId === userId) && users.push({ userId, socketId });
+};
+
+const getUser = (userId) => {
+	return users.find((user) => user.userId === userId);
+};
+
+io.on('connection', (socket) => {
+	console.log(socket.id + ' a user conected');
+	socket.on('addUser', (userId) => {
+		addUser(userId, socket.id);
+	});
+	io.emit('getUsers', users);
+
+	socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+		const receiver = getUser(receiverId);
+		if (receiver) {
+			io.to(receiver.socketId).emit('getMessage', {
+				senderId,
+				receiverId,
+				text
+			});
+		}
+		const myself = getUser(senderId);
+		io.to(myself.socketId).emit('myMessage', {
+			senderId,
+			receiverId,
+			text
+		});
+	});
 
 
-let users = []
-
-const addUser = (userId, socketId)=>{
-	!users.some(user=>user.userId===userId) && users.push({userId, socketId})
-}
-
-
-io.on("connection", socket =>{
-	console.log(socket.id + "a user conected")
-	socket.on("addUser", userId=>{
-		addUser(userId, socket.id)
+	socket.on('disconnect', ()=>{
+		console.log(socket.id, " a user disconnected")
 	})
-	io.emit("getUsers", users)
-})
+});
 
-
-
-server.listen(server.get('port'), () => console.log(`I'm in http://localhost:${server.get('port')}`));
-
-
+ServerHTTP.listen(server.get('port'), () => console.log(`I'm in http://localhost:${server.get('port')}`));
